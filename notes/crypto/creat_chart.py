a9c72ed532c3ccbee166e40dc610e729b868c905
marcusyh <marcusyh@gmail.com>
2015-08-24 02:53:43 +0000

    poloniex chart fix

---
+++ notes/crypto/creat_chart.py
@@ -0,0 +1,56 @@
+import urllib2
+import json
+
+src = urllib2.urlopen('https://poloniex.com/public?command=returnTicker')
+dt  = src.read()
+src.close()
+
+dct = json.loads(dt)
+price = float(dct['BTC_ETH']['last'])
+
+src = urllib2.urlopen('https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH&depth=100000')
+dt  = src.read()
+src.close()
+
+dct = json.loads(dt)
+
+bids = dct['bids']
+asks = dct['asks']
+
+
+bids_real = []
+sum_btc = 0
+sum_product = 0
+for x in bids:
+    cur_price = float(x[0])
+    cur_want  = x[1]
+    cur_btc   = cur_price * cur_want
+    cur_product = cur_btc / price
+    sum_btc   += cur_btc
+    sum_product += cur_product
+    bids_real.append([cur_price, cur_btc, sum_btc, cur_product, sum_product])
+
+asks_real = []
+sum_btc = 0
+sum_product = 0
+for x in asks:
+    cur_price    = float(x[0])
+    cur_product  = x[1]
+    cur_btc      = price * cur_product
+    sum_btc     += cur_btc
+    sum_product += cur_product
+    asks_real.append([cur_price, cur_btc, sum_btc, cur_product, sum_product])
+
+bids_real.reverse()
+real_list = bids_real + asks_real 
+
+range = 0.1
+while range <= 2:
+    hdlr = open('/tmp/test%s' %(int(range*100 + 0.1)), 'w')
+    for x in real_list:
+        if x[0] <= price * (1 - range) or x[0] >= price * (1 + range):
+            continue
+        hdlr.write('\t'.join(str(y) for y in x) + '\n')
+    hdlr.close()
+    range += 0.1
+
