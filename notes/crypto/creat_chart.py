d4e282f8a52974d453466da47c8285923c885a1e
xieyiqiang <marcusyh@gmail.com>
2015-09-21 19:23:22 +0800

    save

--- notes/crypto/creat_chart.py
+++ notes/crypto/creat_chart.py
@@ -2,7 +2,10 @@ import urllib2
 import json
 import sys
 
-flag = 'BTC_%s' %(sys.argv[1].upper())
+MAX = 1000000
+
+crypto = sys.argv[1]
+flag = 'BTC_%s' %(crypto.upper())
 
 src = urllib2.urlopen('https://poloniex.com/public?command=returnTicker')
 dt  = src.read()
@@ -24,32 +27,40 @@ asks = dct['asks']
 bids_real = []
 sum_btc = 0
 sum_product = 0
+sum_count = 0
 for x in bids:
     cur_price = float(x[0])
     cur_want  = x[1]
     cur_btc   = cur_price * cur_want
+    if cur_btc >= MAX:
+        continue
     cur_product = cur_btc / price
     sum_btc   += cur_btc
     sum_product += cur_product
-    bids_real.append([cur_price, cur_btc, sum_btc, cur_product, sum_product])
+    sum_count += 1
+    bids_real.append([cur_price, 1, sum_count, cur_btc, sum_btc, cur_product, sum_product])
 
 asks_real = []
 sum_btc = 0
 sum_product = 0
+sum_count = 0
 for x in asks:
     cur_price    = float(x[0])
     cur_product  = x[1]
     cur_btc      = price * cur_product
+    if cur_btc >= MAX:
+        continue
     sum_btc     += cur_btc
     sum_product += cur_product
-    asks_real.append([cur_price, cur_btc, sum_btc, cur_product, sum_product])
+    sum_count   += 1
+    asks_real.append([cur_price, 1, sum_count, cur_btc, sum_btc, cur_product, sum_product])
 
 bids_real.reverse()
 real_list = bids_real + asks_real 
 
 range = 0.1
-while range <= 2:
-    hdlr = open('/tmp/test%s' %(int(range*100 + 0.1)), 'w')
+while range <= 10:
+    hdlr = open('/tmp/%s%s' %(crypto, int(range*100 + 0.1)), 'w')
     for x in real_list:
         if x[0] <= price * (1 - range) or x[0] >= price * (1 + range):
             continue
