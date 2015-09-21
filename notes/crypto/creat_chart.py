96234a1a157325c6d920c362a7e70709469a1cb3
marcusyh <marcusyh@gmail.com>
2015-09-21 16:06:59 +0000

    recode

--- notes/crypto/creat_chart.py
+++ notes/crypto/creat_chart.py
@@ -2,7 +2,70 @@ import urllib2
 import json
 import sys
 
-MAX = 1000000
+MAX = 0.02
+MIN = 0.000005
+RULE = 0.1
+
+
+def get_raw(flag, price, source):
+    raw_list = []
+    sum_btc = 0
+    sum_product = 0
+    for x in source:
+        cur_price    = float(x[0])
+        cur_source   = x[1]
+        if 'bids' == flag:
+            cur_btc      = cur_price * cur_source
+            cur_product  = cur_btc / price
+        else:
+            cur_product  = cur_source
+            cur_btc      = price * cur_product
+        sum_btc     += cur_btc
+        sum_product += cur_product
+        raw_list.append([cur_price, cur_btc, sum_btc, cur_product, sum_product])
+    return raw_list
+
+def remove_fate(dlist):
+    raw_sum = dlist[-1][2]
+    print raw_sum
+    cur_sum = 0
+    for d in dlist:
+        if d[1] > raw_sum * MAX:
+            d.append(d[1] * -0.1)
+            d.append(cur_sum)
+            continue
+        cur_sum += d[1]
+        d.append(d[1])
+        d.append(cur_sum)
+
+def mark_robot(dlist):
+    trust_sum = dlist[-1][6]
+    for d in dlist:
+        if d[1] < trust_sum * MIN:
+            d[5] *= -10
+
+def add_count(price, dlist):
+    delta_sum   = 0
+    delta_count = 0
+
+    delta_price = price * RULE 
+    for d in dlist:
+        if abs(d[0] - price) > delta_price:
+            break
+        if d[5] < 0:
+            continue
+        delta_count += 1
+        delta_sum   += d[5]
+
+    avg_price = delta_sum /delta_count
+
+    count = 0
+    for d in dlist:
+        count += 1
+        d.append(delta_sum * count / delta_count)
+        d.append(avg_price if d[5] >= 0 else d[5])
+
+
 
 crypto = sys.argv[1]
 flag = 'BTC_%s' %(crypto.upper())
@@ -20,43 +83,17 @@ src.close()
 
 dct = json.loads(dt)
 
-bids = dct['bids']
-asks = dct['asks']
+bids = get_raw('bids', price, dct['bids'])
+asks = get_raw('asks', price, dct['asks'])
+remove_fate(bids)
+remove_fate(asks)
+mark_robot(bids)
+mark_robot(asks)
+add_count(price, bids)
+add_count(price, asks)
 
-
-bids_real = []
-sum_btc = 0
-sum_product = 0
-sum_count = 0
-for x in bids:
-    cur_price = float(x[0])
-    cur_want  = x[1]
-    cur_btc   = cur_price * cur_want
-    if cur_btc >= MAX:
-        continue
-    cur_product = cur_btc / price
-    sum_btc   += cur_btc
-    sum_product += cur_product
-    sum_count += 1
-    bids_real.append([cur_price, 1, sum_count, cur_btc, sum_btc, cur_product, sum_product])
-
-asks_real = []
-sum_btc = 0
-sum_product = 0
-sum_count = 0
-for x in asks:
-    cur_price    = float(x[0])
-    cur_product  = x[1]
-    cur_btc      = price * cur_product
-    if cur_btc >= MAX:
-        continue
-    sum_btc     += cur_btc
-    sum_product += cur_product
-    sum_count   += 1
-    asks_real.append([cur_price, 1, sum_count, cur_btc, sum_btc, cur_product, sum_product])
-
-bids_real.reverse()
-real_list = bids_real + asks_real 
+bids.reverse()
+real_list = bids + asks
 
 range = 0.1
 while range <= 10:
@@ -67,4 +104,3 @@ while range <= 10:
         hdlr.write('\t'.join(str(y) for y in x) + '\n')
     hdlr.close()
     range += 0.1
-
