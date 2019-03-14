459f09f5a8f8bbf8b9fec4e56ba54302a63f33b3
marcusyh <test@DESKTOP-733HNO2.localdomain>
2019-03-14 19:03:34 +0900

    aa

--- notes/japanese/kanji/read_jinmei.py
+++ notes/japanese/kanji/read_jinmei.py
@@ -1,4 +1,7 @@
 import urllib3
+import json
+import traceback
+import re
 from util_read import readfile
 
 #https://ja.wiktionary.org/w/api.php?action=parse&page=%E5%8F%B6&noimages=true&format=json&prop=sections
@@ -7,7 +10,7 @@ from util_read import readfile
 class Jinmei():
     _url = 'https://ja.wiktionary.org/w/api.php'
     _fields = {'action': 'parse', 
-            'page': '叶', 
+            'page': '', 
             'noimages': True, 
             'format': 'json', 
             'prop': 'sections'}
@@ -16,17 +19,34 @@ class Jinmei():
         self.__http = urllib3.PoolManager()
 
     def __fectch_sections(self, kanji):
-        fields = {x: y for x, y in self._fields.values()}
-        fields['page'] = kanji
-        hdlr = self.__http.request('GET', fields=fields)
+        try:
+            fields = {x: y for x, y in self._fields.values()}
+            fields['page'] = kanji
+            hdlr = self.__http.request('GET', fields=fields)
+            if hdlr.status != 200:
+                raise Exception('Return value is not 200')
+            rslt = json.loads(hdlr.data.decode('utf-8'))['parse']['sections']
+            index = 0
+            for item in rslt:
+                if item['anchor'] == '発音':
+                    return index
+                index += 1
+        except Exception:
+            traceback.print_exc()
 
     def __fectch_youmi(self, kanji, index):
         fields = {x: y for x, y in self._fields.values()}
-        fields['page'] = kanji
-        fields['prop'] = 'wikitext'
-        fields['section'] = index
-        hdlr = self.__http.request('GET', fields=fields)
-
+        fields.update({'page': kanji, 'prop': 'wikitext', section: index})
+        try:
+            hdlr = self.__http.request('GET', fields=fields)
+            rslt = json.loads(hdlr.data.decode('utf-8'))['parse']['wikitext']
+            index = 0
+            for item in rslt:
+                if item['anchor'] == '発音':
+                    return index
+                index += 1
+        except Exception:
+            traceback.print_exc()
 
     def fetch_section(kanji):
         hdlr = urllib3.Pool
